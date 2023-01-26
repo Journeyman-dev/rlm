a3066f861a5850d27db8de7a31b74c451d9eef96
Daniel Aimé Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2023-01-26 16:30:09 -0500

    Decl box2 border getter funcs
    The impl are TODO.

---
+++ include/rlm/cellular/borders.hpp
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: 2023 Daniel Aimé Valcour <fosssweeper@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+/*
+    Copyright (c) 2023 Daniel Aimé Valcour
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef RLM_CELLULAR_BORDERS_HPP
+#define RLM_CELLULAR_BORDERS_HPP
+
+#include <rlm/cellular/border_corners.hpp>
+#include <rlm/rotation_motion.hpp>
+
+namespace rl
+{
+    template<rl::signed_integral I>
+    struct segment2;
+    template<rl::signed_integral I>
+    struct box2;
+
+    template<rl::signed_integral I = int>
+    constexpr rl::segment2<I> left_border(
+        const rl::box2<I>& box,
+        rl::BorderCorners border_corners = rl::BorderCorners::All,
+        rl::RotationMotion rotation_motion = rl::RotationMotion::Clockwise
+    ) noexcept;
+
+    template<rl::signed_integral I = int>
+    constexpr rl::segment2<I> right_border(
+        const rl::box2<I>& box,
+        rl::BorderCorners border_corners = rl::BorderCorners::All,
+        rl::RotationMotion rotation_motion = rl::RotationMotion::Clockwise
+    ) noexcept;
+
+    template<rl::signed_integral I = int>
+    constexpr rl::segment2<I> top_border(
+        const rl::box2<I>& box,
+        rl::BorderCorners border_corners = rl::BorderCorners::All,
+        rl::RotationMotion rotation_motion = rl::RotationMotion::Clockwise
+    ) noexcept;
+
+    template<rl::signed_integral I = int>
+    constexpr rl::segment2<I> bottom_border(
+        const rl::box2<I>& box,
+        rl::BorderCorners border_corners = rl::BorderCorners::All,
+        rl::RotationMotion rotation_motion = rl::RotationMotion::Clockwise
+    ) noexcept;
+}
+
+#include <rlm/cellular/detail/borders.inl>
+
+#endif
