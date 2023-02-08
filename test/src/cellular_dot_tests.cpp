fafab63eec90db5ca8f5b0230e540c726cae3e1d
Daniel Aimé Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2023-02-07 22:28:10 -0500

    Add rl::dot and rl::unit_dot unit tests

--- test/src/cellular_dot_tests.cpp
+++ test/src/cellular_dot_tests.cpp
@@ -22,3 +22,115 @@
 
 #include <catch2/catch_all.hpp>
 #include <rlm/cellular/dot.hpp>
+
+SCENARIO("The dot product is gotten between two point2")
+{
+    GIVEN("Two point2")
+    {
+        const rl::point2 point_a(1, 2);
+        const rl::point2 point_b(2, 4);
+        WHEN("The dot product of the two point2 is gotten")
+        {
+            const auto dot = rl::dot(point_a, point_b);
+            THEN("The value is correct")
+            {
+                CHECK(dot == 10);
+            }
+        }
+    }
+}
+
+SCENARIO("The unit dot product is gotten between two point2")
+{
+    GIVEN("Two point2")
+    {
+        const rl::point2 point_a(1, 2);
+        const rl::point2 point_b(2, 4);
+        WHEN("The unit dot product of the two point2 is gotten")
+        {
+            const auto unit_dot = rl::unit_dot(point_a, point_b);
+            THEN("The value is correct")
+            {
+                CHECK(unit_dot == 2.0f);
+            }
+        }
+    }
+}
+
+SCENARIO("The dot product is gotten between a point2 and a segment2")
+{
+    GIVEN("A point2 and a segment2")
+    {
+        const rl::point2 point(1, 2);
+        const rl::segment2 segment(2, 4, 5, 6);
+        WHEN("The dot product of the shapes is gotten")
+        {
+            const auto dot = rl::dot(point, segment);
+            const auto dot_alt = rl::dot(segment, point);
+            THEN("The value is correct")
+            {
+                CHECK(dot == -7);
+            }
+            THEN("A call with the reverse argument order gives the same result")
+            {
+                CHECK(dot == dot_alt);
+            }
+        }
+    }
+}
+
+SCENARIO("The unit dot product is gotten between a point2 and a segment2")
+{
+    GIVEN("A point2 and a segment2")
+    {
+        const rl::point2 point(1, 2);
+        const rl::segment2 segment(2, 4, 5, 6);
+        WHEN("The unit dot product of the shapes is gotten")
+        {
+            const auto unit_dot = rl::unit_dot(point, segment);
+            const auto unit_dot_alt = rl::unit_dot(segment, point);
+            THEN("The value is correct")
+            {
+                CHECK(unit_dot == -1.4f);
+            }
+            THEN("A call with the reverse argument order gives the same result")
+            {
+                CHECK(unit_dot == unit_dot_alt);
+            }
+        }
+    }
+}
+
+SCENARIO("The dot product is gotten between two segment2")
+{
+    GIVEN("Two segment2")
+    {
+        const rl::segment2 segment_a(1, 2, 3, 4);
+        const rl::segment2 segment_b(2, 4, 5, 6);
+        WHEN("The dot product of the two segment2 is gotten")
+        {
+            const auto dot = rl::dot(segment_a, segment_b);
+            THEN("The value is correct")
+            {
+                CHECK(dot == 10);
+            }
+        }
+    }
+}
+
+SCENARIO("The unit dot product is gotten between two segment2")
+{
+    GIVEN("Two segment2")
+    {
+        const rl::segment2 segment_a(1, 2, 3, 4);
+        const rl::segment2 segment_b(2, 4, 5, 6);
+        WHEN("The unit dot product of the two segment2 is gotten")
+        {
+            const auto unit_dot = rl::unit_dot(segment_a, segment_b);
+            THEN("The value is correct")
+            {
+                CHECK(unit_dot == 10.0f);
+            }
+        }
+    }
+}
